e31aad68a9c5a7998079664652da3f9a780a10a8
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-07-15 18:00:34 +0530

    dhgghfhfhdsd'

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -6,13 +6,13 @@ const Search = () => {
 
   const [search,setSearch]=useState("");
   return (
-    <div className={`${style.searchBody}`}>
+    <div className={`${style.searchBody} `}>
       {/* <div className='col-12 mt-4'>
         <div className='form-group'>
           <div className='d-flex justify-content-between '> <input type="text" className='form-control border border-2 border-dark' placeholder='Search' onChange={(e) => setSearch(e.currentTarget.value)} onKeyUp={(e) => onEnter(e)} />  <button className='btn btn-primary bg-dark ms-2'>Search</button></div>
         </div>
       </div> */}
-      <div className=' mt-3'>
+      <div className=''>
         <div className={`${search ? style.searchBoxClick : style.searchBox}`}>
           <input onChange={(e)=>setSearch(e.currentTarget.value)} name='search' value={search} type="text" placeholder="Search..." />
           {/* <button type="submit">Search</button> */}
