2aaedc9283ffc0371beddca6a43cccaabeb43c9b
ManzarBilal <manzarbilal@gmail.com>
2023-07-15 18:03:15 +0530

    gfds

--- src/pages/common/Header.js
+++ src/pages/common/Header.js
@@ -118,7 +118,7 @@ function Header() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            LY3LEY
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages?.map((page) => (
