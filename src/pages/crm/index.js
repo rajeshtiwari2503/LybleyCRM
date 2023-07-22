769db507eca5e59fadbae5dd3ca8f9f5f72cce55
ManzarBilal <manzarbilal@gmail.com>
2023-07-22 17:49:18 +0530

    gfd

--- src/pages/crm/index.js
+++ src/pages/crm/index.js
@@ -1,52 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import SignIn from './sign-in';
+import SignUp from './sign-up';
 
 const CRM = () => {
+    const [view ,setView]=useState("");
+    const handlePage=(val)=>{
+       setView(val);
+    }
+    const handleSideBar=()=>{
+       let sideBar=document.getElementById("wrapper");
+       sideBar.classList.toggle('toggled');
+    }
     return (
         <div>
             <div id="wrapper">
-                <aside id="sidebar-wrapper">
+                
+                <aside id="sidebar-wrapper" >
                     <div className="sidebar-brand">
                         <h2>Logo</h2>
                     </div>
                     <ul className="sidebar-nav">
-                        <li className="active">
-                            <a href="#">
-                                <i className="fa fa-home" />
-                                Home
+                        <li className={view==="" && "active"}>
+                            <a href="#" className='d-flex align-items-center ps-3' onClick={()=>handlePage("")}>
+                                 <DashboardIcon />
+                               <span className=''> Home</span>
                             </a>
                         </li>
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-plug" />
-                                Plugins
+                        <li className={view==="Brand" && "active"}>
+                        <a href="#" className="d-flex align-items-center ps-3" onClick={()=>handlePage("Brand")}>
+                                 <DashboardIcon />
+                               <span className=''>Brand</span>
                             </a>
                         </li>
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-user" />
-                                Users
+                        <li className={view==="Service" && "active"}>
+                        <a href="#" className="d-flex align-items-center ps-3" onClick={()=>handlePage("Service")}>
+                                 <DashboardIcon />
+                               <span className=''>Service</span>
                             </a>
                         </li>
                     </ul>
                 </aside>
+                
                 <div id="navbar-wrapper">
                     <nav className="navbar navbar-inverse">
                         <div className="container-fluid">
-                            <div className="navbar-header">
-                                <a href="#" className="navbar-brand" id="sidebar-toggle">
-                                    <i className="fa fa-bars" />ehhgehg
-                                </a>
-                            </div>
+                            <button className="navbar-toggler" type="button" onClick={handleSideBar}>
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
                         </div>
+                        <div class="container-fluid">
+        <div class="navbar-header">
+          <a href="#" class="navbar-brand" id="sidebar-toggle"><i class="fa fa-bars"></i></a>
+        </div>
+      </div>
                     </nav>
                 </div>
                 <section id="content-wrapper">
                     <div className="row">
                         <div className="col-lg-12">
-                            <h2 className="content-title">Test</h2>
-                            <p>Lorem ipsum...</p>
+                           {view==="Brand" ? <SignIn /> : view==="Service" ?
+                            <SignUp /> : "" }
                         </div>
                     </div>
                 </section>
