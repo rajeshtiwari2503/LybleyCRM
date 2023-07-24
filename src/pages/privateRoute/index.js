63b7402f108bcf7c108307d971b8b2e19a2c7a4d
ManzarBilal <manzarbilal@gmail.com>
2023-07-24 17:22:02 +0530

    fds

---
+++ src/pages/privateRoute/index.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+// import { useAuth } from '../utils/authContext';
+
+const PrivateRoute = ({ children }) => {
+  const router = useRouter();
+//   const { user } = useAuth();
+const [user,setUser]=useState("")
+
+
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+        const user=localStorage.getItem("user1");
+        setUser(user);
+        if (user!=="ABC") {
+            router.push('/crm/sign-in'); // Redirect to the login page if the user is not authenticated.
+            localStorage.removeItem("user1");
+          }
+      }
+  }, [router]);
+
+  if (user==="ABC") {
+    return <>{children}</>;
+  }
+
+   // Render the children (e.g., the "/checkout" page) if the user is authenticated.
+};
+
+export default PrivateRoute;
