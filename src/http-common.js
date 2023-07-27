ff8ea1aef090e5ad7589114ca26a68ede412fba4
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-07-27 12:36:26 +0530

    dbhhhhhhhhhhhhhh

---
+++ src/http-common.js
@@ -0,0 +1,27 @@
+import axios from "axios"
+
+const baseUrl= "http://localhost:5000"
+//hosted on railway
+// const baseUrl="https://sparetradebackend-production.up.railway.app"
+
+const get=(url)=>{
+    return axios.get(baseUrl+url)
+}
+
+const post=(url,obj)=>{
+    return axios.post(baseUrl+url ,obj)
+ }
+
+ const patch=(url,obj)=>{
+    return axios.patch(baseUrl+url ,obj)
+ }
+
+ const put=(url,obj)=>{
+    return axios.put(baseUrl+url ,obj)
+ }
+ const deleteData=(url)=>{
+    return axios.delete(baseUrl+url)
+}
+export default {
+   get ,post,patch,put,deleteData,
+}
